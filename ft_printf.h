de0648bbf04c7f777c6539651acb1c4585bcdff3
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-02-02 16:20:38 +0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -14,14 +14,14 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-
 int	ft_printf(const char *str, ...);
 int	ft_putchar(char c);
 int	ft_print_str(char *str);
 int	ft_int(int number);
 int	ft_u_int(unsigned int number);
-int	ft_hex_nbr(unsigned int number, char c);
+int	ft_hex_nbr(unsigned long long int number, char c);
 int	ft_point(unsigned long long a, int sign);
-int	ft_print_data(va_list *args, const char c);
+int	ft_format(va_list *args, const char c);
+int	ft_flag_check(char str);
 
 #endif
