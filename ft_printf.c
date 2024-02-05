6011b59945cd9617976edab162223595220630df
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-02-05 13:33:32 +0300

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -10,6 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include <unistd.h>
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int	ft_flag_check(char str)
 {
 	return (str == 'c' || str == 's' || str == 'p' || str == 'd' || str == 'i'
@@ -28,45 +36,38 @@ int	ft_format(va_list *args, const char c)
 		return (ft_print_str(va_arg ((*args), char *)));
 	else if (c == 'u')
 		return (ft_u_int(va_arg ((*args), unsigned int)));
-	else if (c == 'x')
-		return (ft_hex_nbr(va_arg((*args), unsigned int), 'a' - 10));
-	else if (c == 'X')
-		return (ft_hex_nbr(va_arg((*args), unsigned int), 'A' - 10));
+	else if (c == 'x' || c == 'X')
+		return (ft_hex_nbr(va_arg((*args), unsigned int), c));
 	else if (c == 'p')
-	{
-		if (write(1, "0x", 2) == -1)
-			return (-1);
-		return (ft_hex_nbr(va_arg((*args), unsigned long int), 'a' - 10) + 2);
-	}
+		return (ft_point(va_arg((*args), unsigned long), 1));
 	else
 		return (0);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	va_list	args;
-	int		count;
-	int		tmp;
-	int		leng;
+	va_list	arg;
+	int		len;
+	int		i;
+	int		value;
 
-	count = -1;
-	leng = 0;
-	va_start (args, str);
-	while (str[++count] != '\0')
+	len = 0;
+	i = -1;
+	va_start(arg, str);
+	while (str[++i])
 	{
-		if (str[count] == '%' && ft_flag_check(str[count + 1]))
+		if (str[i] == '%' && ft_flag_check(str[i + 1]))
 		{
-			tmp = ft_format(&args, str[++count]);
-			if (tmp == -1)
+			value = ft_format(&arg, str[++i]);
+			if (value == -1)
 				return (-1);
-			leng += tmp - 1;
+			len += value;
 		}
-		else if (str[count] == '%')
-			leng--;
-		else if (ft_putchar(str[count]) == -1)
+		else if (str[i] != '%' && ft_putchar(str[i]) == -1)
 			return (-1);
-		leng++;
+		else if (str[i] != '%')
+			len++;
 	}
-	va_end(args);
-	return (leng);
+	va_end(arg);
+	return (len);
 }
